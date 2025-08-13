0f20ece9a9d7fdeafa16ba6733e158c8c5168a4f
leoarantes14 <leonardo.arantes.miguel@escola.pr.gov.br>
2025-08-13 07:34:05 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,9 @@
+criaCartao(
+  'Espote'
+  'O que é esporte?'
+  'O esporte é uma atividade física que envolve regras, competição e habilidade. Ele pode ser praticado de forma individual ou em equipe, e geralmente tem como objetivo a superação de limites, o aprimoramento de capacidades físicas e mentais,e a busca pela vitória.'
+)
 
+criaCartao(
+  'Esporte'
+)
