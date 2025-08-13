c41927e091379097b95cfa30dc5695ec464ced2b
leoarantes14 <leonardo.arantes.miguel@escola.pr.gov.br>
2025-08-13 07:40:18 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,17 @@
 criaCartao(
-  'Espote'
+  'Esportes'
   'O que é esporte?'
   'O esporte é uma atividade física que envolve regras, competição e habilidade. Ele pode ser praticado de forma individual ou em equipe, e geralmente tem como objetivo a superação de limites, o aprimoramento de capacidades físicas e mentais,e a busca pela vitória.'
 )
 
 criaCartao(
-  'Esporte'
+  'Esportes'
   'Quanto sportes existem?'
   'existem mais de 8.000 esportes e jogos esportivos em todo o mundo. Isso inclui desde as modalidades mais conhecidas e olímpicas até as regionais, folclóricas e recreativas.'
 )
 
 criaCartao(
-  'Esporte'
+  'Esportes'
   'O que são as Olímpiadas?'
   'As Olimpíadas, ou Jogos Olímpicos, são o maior evento multiesportivo do mundo, reunindo atletas de praticamente todos os países para competir em diversas modalidades. Eles são um símbolo global de união, paz e superação, celebrando a excelência física e o espírito esportivo.'  
 )
