76c6829d022ef1573843c159bf6f1bc059d5c7b3
leoarantes14 <leonardo.arantes.miguel@escola.pr.gov.br>
2025-08-20 07:39:59 -0300

    correção

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-  'Esportes'
-  'O que é esporte?'
+  'Esportes',
+  'O que é esporte?',
   'O esporte é uma atividade física que envolve regras, competição e habilidade. Ele pode ser praticado de forma individual ou em equipe, e geralmente tem como objetivo a superação de limites, o aprimoramento de capacidades físicas e mentais,e a busca pela vitória.'
 )
 
 criaCartao(
-  'Esportes'
-  'Quanto sportes existem?'
+  'Esportes',
+  'Quanto sportes existem?',
   'existem mais de 8.000 esportes e jogos esportivos em todo o mundo. Isso inclui desde as modalidades mais conhecidas e olímpicas até as regionais, folclóricas e recreativas.'
 )
 
 criaCartao(
-  'Esportes'
-  'O que são as Olímpiadas?'
+  'Esportes',
+  'O que são as Olímpiadas?',
   'As Olimpíadas, ou Jogos Olímpicos, são o maior evento multiesportivo do mundo, reunindo atletas de praticamente todos os países para competir em diversas modalidades. Eles são um símbolo global de união, paz e superação, celebrando a excelência física e o espírito esportivo.'  
 )
 
 criaCartao(
-  'Esportes'
-  'Qual o país com mais conquistas nas Olímpiadas?'
+  'Esportes',
+  'Qual o país com mais conquistas nas Olímpiadas?',
   'Atualmente o país com mais conquistas é os Estado Unidos com 2.980 medalhas'
 )
 
 criaCartao(
-  'Esportes'
-  'Ranking dos Estados Unidos nas Olímpiadas de Paris 2024'
+  'Esportes',
+  'Ranking dos Estados Unidos nas Olímpiadas de Paris 2024',
   'Os EUA ficaram em primeiro no Ranking das medalhas, com 126 medalhas'
 )
 
 criaCartao(
-  'Esportes'
-  'Ranking do Brasil nas Olímpiadas de Paris 2024'
-  'O desempenho do Brasil (foi o 20º no ranking, com 3 ouros, 7 pratas e 10 bronzes)
+  'Esportes',
+  'Ranking do Brasil nas Olímpiadas de Paris 2024',
+  'O desempenho do Brasil (foi o 20º no ranking, com 3 ouros, 7 pratas e 10 bronzes)'
 )
